8b9c51522f3be2a2de9f9ff0d9d74398c692d83a
Kevin Hyde <kphyde@gmail.com>
2024-07-25 10:23:52 -0700

    add if example

--- 04-Intro_to_Vue/lesson-app/src/App.vue
+++ 04-Intro_to_Vue/lesson-app/src/App.vue
@@ -2,9 +2,13 @@
 import { ref } from "vue";
 
 const ingredients = ref(["meat", "cheese", "veggies"]);
+
+const show = ref(true)
 </script>
 <template>
   <main>
+    <button @click='show = !show">Toggle paragraph visibility</button>
+    <p v-if='show'>Can you see me?</p>
     <ul>
       <li v-for="ingredient in ingredients" :key="ingredient">
         {{ ingredient }}
