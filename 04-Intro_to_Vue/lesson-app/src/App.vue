f50087907fbdb7a4d6c8ae1ebeb7e760b33a7726
Kevin Hyde <kphyde@gmail.com>
2024-07-25 10:29:46 -0700

    add object loop example

--- 04-Intro_to_Vue/lesson-app/src/App.vue
+++ 04-Intro_to_Vue/lesson-app/src/App.vue
@@ -1,17 +1,36 @@
 <script setup>
 import { ref } from "vue";
 
+// Conditional rendering
+const show = ref(true);
+
+// Loop over Array
 const ingredients = ref(["meat", "cheese", "veggies"]);
 
-const show = ref(true)
+// Loop over Array of Objects
+const people = ref([
+  { name: "John", age: 30 },
+  { name: "Jane", age: 25 },
+  { name: "Susan", age: 41 },
+]);
 </script>
 <template>
   <main>
-    <button @click='show = !show">Toggle paragraph visibility</button>
-    <p v-if='show'>Can you see me?</p>
+    <!-- Conditional rendering -->
+    <button @click="show = !show">Toggle paragraph visibility</button>
+    <p v-if="show">Can you see me?</p>
+
+    <!-- Loop over Array -->
     <ul>
       <li v-for="ingredient in ingredients" :key="ingredient">
         {{ ingredient }}
+      </li>
+    </ul>
+
+    <!-- Loop over Array of Objects -->
+    <ul>
+      <li v-for="person in people" :key="person.name">
+        {{ person.name }} is {{ person.age }} years old.
       </li>
     </ul>
   </main>
