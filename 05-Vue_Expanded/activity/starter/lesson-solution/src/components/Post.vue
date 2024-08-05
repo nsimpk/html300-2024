895aef2c4c3ab81e47d248edefecc9e36c91f939
Kevin Hyde <kphyde@gmail.com>
2024-08-04 18:13:30 -0700

    code from video

---
+++ 05-Vue_Expanded/activity/starter/lesson-solution/src/components/Post.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { defineProps, ref } from 'vue'
+
+const props = defineProps({
+  id: Number,
+  author: String,
+  title: String,
+  category: String,
+  excerpt: String,
+  likes: Number
+})
+
+const likes = ref(props.likes)
+
+const incerementLikes = () => {
+  likes.value++
+}
+</script>
+
+<template>
+  <div class="post">
+    <h2>{{ props.title }}</h2>
+    <p>By: {{ props.author }}</p>
+    <p>Category: {{ props.category }}</p>
+    <p>{{ props.excerpt }}</p>
+    <p>Likes: {{ likes }}</p>
+    <button @click="incerementLikes">Like</button>
+  </div>
+</template>
+
+<style scoped>
+.post {
+  border: 1px solid #ccc;
+  padding: 1rem;
+  margin: 1rem 0;
+}
+</style>
